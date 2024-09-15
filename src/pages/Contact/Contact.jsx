9701acd73d74bdaddc85d170b93e773d1e2f35ef
Anurag Yadav <anurag192000@gmail.com>
2024-09-15 11:30:40 +0530

    responsive

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,73 +1,38 @@
 import React from 'react'
-import { FaFacebookSquare } from "react-icons/fa";
-import { IoLogoYoutube } from "react-icons/io5";
-import { BsInstagram } from "react-icons/bs";
-import { FaLinkedin } from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi";
-import { FaLocationDot } from "react-icons/fa6";
-import { FaPhoneAlt } from "react-icons/fa";
-import { BsGlobeCentralSouthAsia } from "react-icons/bs";
 
 const Contact = () => {
   return (
     <>
-      <div className='text-yellow-400 '>
-        <h1 className='text-5xl font-bold text-center mb-16'>Contact</h1>
-        <div className='flex space-x-28 justify-center'>
-            <div className='flex-row space-y-6'>
-                <div className='space-y-2'>
-                    <div className='flex space-x-2 font-bold'><FaLocationDot/><span>ADDRESS</span></div>
-                    <div className='w-60 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, architecto.</div>
-                </div>
-                <div className='space-y-2'>
-                    <div className='flex space-x-2 font-bold'><HiOutlineMail className='text-2xl'/><span>EMAIL</span></div>
-                    <div className='text-sm'>example@gmail.com</div>
-                </div>
-                <div className='space-y-2'>
-                    <div className='flex space-x-2 font-bold'><FaPhoneAlt className='text-sm mt-1.5'/><span>PHONE</span></div>
-                    <div className='text-sm'>000-001-345-67890</div>
-                </div>
-                <div className='space-y-4'>
-                <div className='flex space-x-2 font-bold'><BsGlobeCentralSouthAsia className='text-lg'/><span>SOCIAL</span></div>
-                    <div className='flex space-x-4 text-xl'><FaFacebookSquare /><BsInstagram className='text-black bg-yellow-400 rounded-full'/><IoLogoYoutube/><FaLinkedin/></div>
-                </div>
-            </div>
-            <div>
-                <form action="">
-                    <div className='grid justify-items-center space-y-4'>
-                        <div className='grid space-y-2 '>
-                            <div>
-                                <label htmlFor="" className='font-bold'>Username</label>                                   
-                            </div>
-                            <div className='border-2 border-yellow-400 p-2 w-custom-input-width rounded-lg'>
-                                <input type="text" placeholder='ex: john doe' className='placeholder-yellow-400 bg-transparent outline-none border-none text-sm' />
-                            </div>
+      <div className='contact'>
+      <div className='contact-container'>
+        <div className='contact-container-heading-box'>
+            <span>Contact</span>
+        </div>
+        <div className='contact-container-form-container'>
+              <form action="">                      
+                        <div className='contact-container-form-container-box'>
+                        <div className='contact-container-form-container-box-input-box'>
+                            <label htmlFor="Name">Name</label>
+                            <input type="text" className='contact-container-form-container-box-input-box-input'/>
                         </div>
-                        <div className='grid space-y-2 '>
-                            <div>
-                                <label htmlFor="" className='font-bold'>Email address</label>                                   
-                            </div>
-                            <div className='flex border-2 border-yellow-400  w-custom-input-width rounded-lg space-x-1 '>
-                                <HiOutlineMail className='text-xl text-black bg-yellow-400 h-full w-10 rounded-l-md '/>
-                                <input type="text" placeholder='example@gmail.com' className='placeholder-yellow-400 p-2 bg-transparent outline-none border-none text-sm' />
-                            </div>
+                        <div className='contact-container-form-container-box-input-box'>
+                            <label htmlFor="Email">Email</label>
+                             <input type="text" className='contact-container-form-container-box-input-box-input' />
+                         </div>
+                         <div className='contact-container-form-container-box-input-box'>
+                            <label htmlFor="description">Description</label>
+                            <textarea name="" id="description" className='contact-container-form-container-box-input-box-textarea' rows={8}></textarea>
                         </div>
-                        <div className='grid space-y-2'>
-                            <div>
-                                <label htmlFor="" className='font-bold'>Message</label>                                   
-                            </div>
-                            <div className='border-2 border-yellow-400 p-2 w-custom-input-width  rounded-lg'>
-                                <textarea name="" id=""  placeholder='feel free to contact us.'className='placeholder-yellow-400 bg-transparent outline-none border-none text-sm w-full h-custom-textarea-height '  ></textarea>
-                            </div>
-                        </div>
-                        <div className='w-custom-button-width bg-yellow-400 text-black p-3 text-center rounded-full '>
-                            <button>Send Message</button>
-                        </div>                                          
-                    </div>   
+                        <div className='contact-container-form-container-box-submit-box'>
+                            <button className='contact-container-form-container-box-submit-box-button'>
+                                Submit
+                            </button>
+                        </div>                                                              
+                    </div>                 
                 </form>
             </div>
-        </div>
-      </div>  
+        </div> 
+      </div> 
     </>
   )
 }
--- src/pages/Create/Create.jsx
+++ src/pages/Create/Create.jsx
@@ -1,55 +1,60 @@
 import React from 'react'
+import { FaImage } from "react-icons/fa";
 
 const Create = () => {
   return (
     <>
-      <div className='text-yellow-400 my-8 space-y-8'>
-        <div className='text-5xl font-bold text-center'>
-            <h1 className=''>Create Post</h1>
+    <div className='create'>
+      <div className='create-container'>
+        <div className='create-container-heading-box'>
+            <span>Create Post</span>
         </div>
-        <div>
+        <div className='create-container-form-container'>
               <form action="">
-                    <div className='grid justify-items-center space-y-4'>
-                        <div className='grid space-y-2 '>
-                            <div>
-                                <label htmlFor="" className='font-bold'>Post Image</label>                                   
+                    <div className='create-container-form-container-outer'>
+                        <div className='create-container-preview-container'>
+                            <div className='create-container-preview-container-image-box'>
+                                <img src="images/mountain.avif" alt="" className='create-container-preview-container-image-box-image'/>
                             </div>
-                            <div className='border-2 border-yellow-400 p-2 w-custom-input-width rounded-lg'>
-                                <input type="file" placeholder='select image for your post' className='placeholder-yellow-400 bg-transparent outline-none border-none text-sm' />
+                            <div className='create-container-preview-container-desc-box'>
+                                <span className='create-container-preview-container-desc-box-heading'>The tale of the Decade</span>
+                                <div className='create-container-preview-container-desc-box-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta atque maxime quos nostrum itaque perspiciatis odio suscipit tempore nemo accusamus incidunt at iste blanditiis inventore, quam corrupti a necessitatibus saepe? Odio voluptatum saepe quisquam! A explicabo illum quia dolorem dolor et suscipit iure, quae, repellat distinctio nemo, voluptatum velit consequuntur deleniti architecto corporis molestias eius excepturi assumenda similique itaque. Consequatur cumque totam debitis! Labore et ab ducimus expedita iusto pariatur, eius perspiciatis nulla molestiae cum iure aperiam minima architecto vitae. Amet non, deleniti modi eveniet qui provident repellat exercitationem nostrum nesciunt ut esse necessitatibus! Unde facere officia ipsum porro consectetur.</div>
                             </div>
+                        </div> 
+                        <div className='create-container-form-container-box'>
+                        <div className='create-container-form-container-box-image-box'>
+                            <span className='create-container-form-container-box-image-box-heading'>Post Image</span>
+                            <label htmlFor="post-image" className='create-container-form-container-box-image-box-image' >      
+                                <span><FaImage  className='create-post-image-icon'/></span>    
+                                <span className='create-post-image-desc'>
+                                    Drag & drop file <br /> or Browse media on your device
+                                </span>                                                    
+                                <input type="file" id='post-image' className='hidden' />
+                            </label>
                         </div>
-                        <div className='grid space-y-2 '>
-                            <div>
-                                <label htmlFor="" className='font-bold'>Title</label>                                   
-                            </div>
-                            <div className='border-2 border-yellow-400 p-2 w-custom-input-width rounded-lg'>
-                                <input type="text" placeholder='post title' className='placeholder-yellow-400 bg-transparent outline-none border-none text-sm' />
-                            </div>
+                        <div className='create-container-form-container-box-input-box'>
+                            <label htmlFor="title">Title</label>
+                            <input type="text" className='create-container-form-container-box-input-box-input'/>
                         </div>
-                        <div className='grid space-y-2 '>
-                            <div>
-                                <label htmlFor="" className='font-bold'>Category</label>                                   
-                            </div>
-                            <div className='border-2 border-yellow-400 p-2 w-custom-input-width rounded-lg'>
-                                <input type="text" placeholder='Category of the post' className='placeholder-yellow-400 bg-transparent outline-none border-none text-sm' />
-                            </div>
+                        <div className='create-container-form-container-box-input-box'>
+                            <label htmlFor="category">Category</label>
+                             <input type="text" className='create-container-form-container-box-input-box-input' />
+                         </div>
+                         <div className='create-container-form-container-box-input-box'>
+                            <label htmlFor="description">Description</label>
+                            <textarea name="" id="description" className='create-container-form-container-box-input-box-textarea' rows={8}></textarea>
                         </div>
-                      
-                        <div className='grid space-y-2'>
-                            <div>
-                                <label htmlFor="" className='font-bold'>Description</label>                                   
-                            </div>
-                            <div className='border-2 border-yellow-400 p-2 w-custom-input-width  rounded-lg'>
-                                <textarea name="" id=""  placeholder='feel free to contact us.'className='placeholder-yellow-400 bg-transparent outline-none border-none text-sm w-full h-custom-textarea-height '  ></textarea>
-                            </div>
-                        </div>
-                        <div className='w-custom-button-width bg-yellow-400 text-black p-3 text-center rounded-full '>
-                            <button>Submit</button>
-                        </div>                                          
-                    </div>   
+                        <div className='create-container-form-container-box-submit-box'>
+                            <button className='create-container-form-container-box-submit-box-button'>
+                                Submit
+                            </button>
+                        </div>                                       
+                        </div>                       
+                    </div>                 
                 </form>
             </div>
-      </div>  
+        </div> 
+      </div> 
     </>
   )
 }
